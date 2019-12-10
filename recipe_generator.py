44cd52c080eb61c9da60e9900f371a33e68bfc37
sarahjpell <32115290+sarahjpell@users.noreply.github.com>
2019-12-09 23:55:33 -0500

    Update recipe_generator.py

--- recipe_generator.py
+++ recipe_generator.py
@@ -1,86 +1,85 @@
-import json
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 23:43:54 2019
+
+@author: sarahpell
+"""
+
+from keras import backend as K
+K.clear_session()
+
 import numpy
 import sys
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-# import nltk
-# nltk.download('stopwords')
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
-# import tensorflow
-import matplotlib as plt
-import pickle
+import pandas as pd
+import numpy as np
 
-##############FUNCTIONS################################
-def tokenize_txt(txt):
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(txt)
-    filtered = filter(lambda token: token not in stopwords.words('english'), tokens)
-    return " ".join(filtered)
+def select(preds, temperature=1.0):
+# helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
 
-########################################################
 
-data_directory = 'recipes'
-directory = os.fsencode(data_directory)  # establish directory
+data1 = pd.read_json(r'recipes_raw_nosource_ar.json')
+# data2 = pd.read_json(r'recipes_raw_nosource_epi.json')
+# data3 = pd.read_json(r'recipes_raw_nosource_fn.json')
+
+X = []
+y = []
 recipes = ''
-ct = 0
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    filename = data_directory + '/' + filename
-    # # don't parse .DS_Store file
-    # if filename != "{}/.DS_Store".format(data_directory):
-    if ct < 1:
-        try:
-            # try parsing as json
-            f = open(filename)
-            data = json.loads(f.read())
-            for k in data.keys():
-                try:
-                    txt = (data[k]["instructions"])
-                    if txt != None:
-                        recipes = recipes + ' ' + txt.rstrip()
-                        ct = ct + 1
-                except KeyError:
-                    pass
-
-            f.close()
-        except FileNotFoundError:
-            pass
-print('here!!!!!!!')
+ct=0
+# len(data1.keys())
+for r1 in data1.keys()[:1000]:
+    if type(data1[r1]['instructions']) is str:
+        recipes += 'instructions: ' + data1[r1]['instructions']
+# for r2 in data2.keys():
+#     if type(data2[r2]['instructions']) is str:
+#         recipes += 'instructions: ' + data2[r2]['instructions'].rstrip()
+# for r3 in data3.keys():
+#     if type(data3[r3]['instructions']) is str:
+#         recipes += 'instructions: ' + data3[r3]['instructions'].rstrip()
+# print(recipes)
+               
+        
+       
+        
 tokenized_recipes = tokenize_txt(recipes)
 
-
-print(tokenized_recipes)
-chars = sorted(list(set(tokenized_recipes)))
-print(chars)
-char_to_num = dict((c, i) for i, c in enumerate(chars))
-print(char_to_num)
+char_list = sorted(list(set(tokenized_recipes)))
+print(char_list)
+char_cts = dict((c, i) for i, c in enumerate(char_list))
+print(char_cts)
 
 input_len = len(tokenized_recipes)
-vocab_len = len(chars)
+num_chars = len(char_list)
 print ("Total number of characters:", input_len)
-print ("Total vocab:", vocab_len)
+print ("Total vocab:", num_chars)
 
 
 seq_length = 100
 Xdata = []
 ydata = []
 
-for i in range(0, input_len - seq_length):
+for i in range(0, input_len - seq_length, 1):
     in_seq = tokenized_recipes[i:i + seq_length]
     out_seq = tokenized_recipes[i + seq_length]
 
-    Xdata.append([char_to_num[char]] for char in in_seq)
-    ydata.append([char_to_num[out_seq]])
+    Xdata.append([char_cts[char] for char in in_seq])
+    ydata.append(char_cts[out_seq])
 
 num_patterns = len(Xdata)
-print(num_patterns)
+print('number of patterns: ', num_patterns)
 
 X = numpy.reshape(Xdata, (num_patterns, seq_length, 1))
-X = X/float(vocab_len)
+X = X/float(num_chars)
 y = np_utils.to_categorical(ydata)
 
 model = Sequential()
@@ -92,44 +91,37 @@ model.add(LSTM(128))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', 
+              optimizer='adam')
 
 
 filepath = "model_weights.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1,
-                             save_best_only=True, mode='min')
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 desired_callbacks = [checkpoint]
 
+scores = model.fit(X, y, epochs=40, batch_size=256, callbacks=desired_callbacks)
 
-scores = model.fit(X, y, epochs = 10, batch_size = 32, callbacks = desired_callbacks, save_scores=True)
-fig, axes = plt.subplots(figsize=(15, 6), ncols=2)
-scores['accuracy'].plot(ax=axes[0], title='Train Accuracy')
-scores['loss'].plot(ax=axes[1], title='Train Loss')
-for ax in axes:
-    ax.set(xlabel='Steps')
+num_to_char = dict((i, c) for i, c in enumerate(char_list))
 
-# filename = "model_weights_saved.hdf5"
-# model.load_weights(filename)
-# model.compile(loss='categorical_crossentropy', optimizer='adam')
-#
-# num_to_char = dict((i, c) for i, c in enumerate(chars))
-#
-# start = numpy.random.randint(0, len(Xdata) - 1)
-# pattern = Xdata[start]
-# print("Random Seed:")
-# print("\"", ''.join([num_to_char[value] for value in pattern]), "\"")
-#
-# for i in range(1000):
-#     x = numpy.reshape(pattern, (1, len(pattern), 1))
-#     x = x / float(vocab_len)
-#     prediction = model.predict(x, verbose=0)
+start = numpy.random.randint(0, len(Xdata) - 1)
+pattern = Xdata[start]
+print("Random Seed:")
+print("\"", ''.join([num_to_char[value] for value in pattern]), "\"")
+
+
+for i in range(1000):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(num_chars)
+    prediction = model.predict(x, verbose=0)[0]
+    diversity = 0.2
+    index = select(prediction, diversity)
 #     index = numpy.argmax(prediction)
-#     result = num_to_char[index]
-#     seq_in = [num_to_char[value] for value in pattern]
-#
-#     sys.stdout.write(result)
-#
-#     pattern.append(index)
-#     pattern = pattern[1:len(pattern)]
+    result = num_to_char[index]
+    seq_in = [num_to_char[value] for value in pattern]
+
+    sys.stdout.write(result)
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
+    
+    
+#model.summary()
