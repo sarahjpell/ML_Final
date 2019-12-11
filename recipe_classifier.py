0eb1c69695759aa393e0d1996ef40f364f68ff1e
sarahjpell <32115290+sarahjpell@users.noreply.github.com>
2019-12-11 00:28:00 -0500

    Update recipe_classifier.py

--- recipe_classifier.py
+++ recipe_classifier.py
@@ -5,11 +5,13 @@ Created on Mon Dec  9 12:41:19 2019
 
 @author: sarahpell
 """
-
+#following guide from: 
+#https://stackabuse.com/text-generation-with-python-and-tensorflow-keras/
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import re
 from keras.preprocessing.text import one_hot
@@ -121,13 +123,57 @@ model.add(Dropout(0.2))
 model.add(Dense(6))
 model.add(Activation('softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#filepath = "recipe_class_model3.hdf5"
+#checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1,
+#                             save_best_only=True, mode='min')
+#desired_callbacks = [checkpoint]
+#
+#
+#filename = "recipe_class_model2.hdf5"
+#model.load_weights(filename)
+#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
+X_train, X_test, y_train, y_test = train_test_split(padded_sentences, y, test_size=0.2)
+history = model.fit(X_train, y_train, epochs=20, verbose=1, callbacks=desired_callbacks, validation_data=(X_test,y_test))
+# loss, accuracy = model.evaluate(padded_sentences, y, verbose=0)
+# print('Accuracy: %f' % (accuracy*100))
+#
+#import pickle
+#file_Name = "history3.pkl"
+## open the file for writing
+#fileObject = open(file_Name,'wb') 
+#pickle.dump(history,fileObject) 
+#
 
-# print(model.summary())
-
-# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
-
-model.fit(padded_sentences, y, epochs=1, verbose=1)
-loss, accuracy = model.evaluate(padded_sentences, y, verbose=0)
-print('Accuracy: %f' % (accuracy*100))
-
+#history1 = pickle.load( open( "history.pkl", "rb" ) )
+#history2 = pickle.load( open( "history2.pkl", "rb" ) )
+#
+#history_acc = []
+#history_loss = []
+#
+#for acc in history1.history['accuracy']:
+##    print(acc)
+#    history_acc.append(acc)
+#    
+#for acc2 in history2.history['accuracy']:
+##    print(acc2)
+#    history_acc.append(acc2)
+##print('-------------------------------')
+#for loss in history1.history['loss']:
+##    print(loss)
+#    history_loss.append(loss)
+#    
+#for loss2 in history2.history['loss']:
+##    print(loss2)
+#    history_loss.append(loss2)
+#
+#fig, axes = plt.subplots(figsize=(13, 4.5), ncols=2)
+##print(history.history.keys())
+#axes[0].plot(history_acc, color='blue', label='accuracy')
+## plt.legend(loc="upper left")
+#axes[1].plot(history_loss, color='orange', label='loss')
+## plt.legend(loc="upper left")
+#for ax in axes:
+#    ax.set(xlabel='Epochs')
+#axes[0].set(ylabel='accuracy')
+#axes[1].set(ylabel='loss')
+#    
